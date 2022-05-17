16f66a623b5bd9de7c8e1539d4ad166af8229c84
student <student@students-Mac-mini-5.local>
2022-05-17 09:07:47 -0600

    Added some testing, visual studio may be compiling as c++ for some reason :/, need to take another look

--- Strongholder/main.c
+++ Strongholder/main.c
@@ -9,17 +9,18 @@
 #include <malloc.h>
 #endif
 
-void testStrongholdAddFloor()
+void testStrongholdAddFloor(Stronghold* nStrong)
 {
-	Stronghold nStrong;
-	initializeStronghold(&nStrong);
-	printf("Highest height: %d\nHighest height + 1: %d\n", nStrong.heighestHeight, nStrong.heighestHeight + 1);
-
-	// addFloor(&nStrong, NewBasement);
-	addFloor(&nStrong, NewBasement);
+	initializeStronghold(nStrong);
+	printf("Highest height: %d\nHighest height + 1: %d\n", nStrong->heighestHeight, nStrong->heighestHeight + 1);
+	addFloor(nStrong, NewFloor);
+	addFloor(nStrong, NewBasement);
+	addFloor(nStrong, NewBasement);
+	/*// addFloor(&nStrong, NewBasement);
+	
 	addFloor(&nStrong, NewFloor);
 	addFloor(&nStrong, NewFloor);
-	addFloor(&nStrong, NewBasement);
+	
 	addFloor(&nStrong, NewFloor);
 	addFloor(&nStrong, NewBasement);
 	addFloor(&nStrong, NewFloor);
@@ -28,13 +29,22 @@ void testStrongholdAddFloor()
 	addFloor(&nStrong, NewFloor);
 	sortFloors(&nStrong);
 	displayStronghold(&nStrong);
+	*/
+
 }
 
+void showStrongholdData(Stronghold* nStrong)
+{
+	printf("Floor size: %ld\nFloor Pointer Size: %ld\n", sizeof(Floor), sizeof(Floor*));
+	printf("Floors: %d\nSpace allocated: %ld\n", nStrong->numFloors, sizeof(Floor) * nStrong->numFloors);
+	printf("size of Int: %ld", sizeof(int));
+}
 
  int main()
 {
-
-	testStrongholdAddFloor();
+	Stronghold nStrong;
+	testStrongholdAddFloor(&nStrong);
+	showStrongholdData(&nStrong);
 
 	return 0;
 }
